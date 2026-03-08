4424009a00b43d13ebdf10d3a2981cb63512c45e
Gladwin-32 <santhosh@lyfskills.co>
2026-03-08 15:30:09 +0530

    added analytivs

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
-
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </StrictMode>,
 )
